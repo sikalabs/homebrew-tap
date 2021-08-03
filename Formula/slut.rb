e2821a5ef67d3dbce77b2dd56d8c1f77d1c98724
goreleaserbot <goreleaser@carlosbecker.com>
2021-08-03 11:37:08 +0200

    Brew formula update for slut version v0.3.0

--- Formula/slut.rb
+++ Formula/slut.rb
@@ -5,24 +5,24 @@
 class Slut < Formula
   desc "SikaLabs Utils"
   homepage "https://github.com/sikalabs/slut"
-  version "0.2.0"
+  version "0.3.0"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/sikalabs/slut/releases/download/v0.2.0/slut_v0.2.0_darwin_amd64.tar.gz"
-    sha256 "3efb7d3e086b0554ab84ab7a7333ab0ff5210c17c7d0f507cb0ed9bb88ac1fa0"
+    url "https://github.com/sikalabs/slut/releases/download/v0.3.0/slut_v0.3.0_darwin_amd64.tar.gz"
+    sha256 "62aa8ea266a9c16bf681c023d3918f852a9479bba8255605efe801a5b1ec332a"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/sikalabs/slut/releases/download/v0.2.0/slut_v0.2.0_darwin_arm64.tar.gz"
-    sha256 "710766e4eed501e4e2ca0bfc976fef19f59dd89eba11e10f4b41a3232728d7f1"
+    url "https://github.com/sikalabs/slut/releases/download/v0.3.0/slut_v0.3.0_darwin_arm64.tar.gz"
+    sha256 "470e24f96e5a649aa9c48e3be0f3bf567997da9a37e59f6fa63fd5af6df4d9fc"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/sikalabs/slut/releases/download/v0.2.0/slut_v0.2.0_linux_amd64.tar.gz"
-    sha256 "af840bb50228c8ec4be384f81d2aeb759fe750e5829ff513cb8989d05d997c12"
+    url "https://github.com/sikalabs/slut/releases/download/v0.3.0/slut_v0.3.0_linux_amd64.tar.gz"
+    sha256 "88f0033d0a9b1baaa92237294ce90440730070678431206604f5a6db5d63c306"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/sikalabs/slut/releases/download/v0.2.0/slut_v0.2.0_linux_arm64.tar.gz"
-    sha256 "3a3714d957fc7be12207cdacd4ebab0c9e477235ee58c9b2854c4fd74669b943"
+    url "https://github.com/sikalabs/slut/releases/download/v0.3.0/slut_v0.3.0_linux_arm64.tar.gz"
+    sha256 "ae23aafe308273f51536cc75c61350bb88883d53d48ad1b042410f7561392e4f"
   end
 
   def install
