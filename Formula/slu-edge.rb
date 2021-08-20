6d1c5b12065bd0e0b31deb5469ee1ee5adb81427
goreleaserbot <goreleaser@carlosbecker.com>
2021-08-20 23:15:41 +0200

    Brew formula update for slu version v0.11.0-dev-2

--- Formula/slu-edge.rb
+++ Formula/slu-edge.rb
@@ -5,28 +5,28 @@
 class SluEdge < Formula
   desc "SikaLabs Utils"
   homepage "https://github.com/sikalabs/slu"
-  version "0.11.0-dev-1"
+  version "0.11.0-dev-2"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/sikalabs/slu/releases/download/v0.11.0-dev-1/slu_v0.11.0-dev-1_darwin_amd64.tar.gz"
-      sha256 "52f3d1a237d3ca655138ba84d067ff66af834401aa3c98e5bc7862d8be656612"
+      url "https://github.com/sikalabs/slu/releases/download/v0.11.0-dev-2/slu_v0.11.0-dev-2_darwin_amd64.tar.gz"
+      sha256 "d9545c36df1cd6880d2dd6297395c3afa6157c3635a67b618c1c3844d4d4a326"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/sikalabs/slu/releases/download/v0.11.0-dev-1/slu_v0.11.0-dev-1_darwin_arm64.tar.gz"
-      sha256 "e350b55211825d4d8b6b9b21a31e7ca8fe21e1175489bebd06c9c6fd29653429"
+      url "https://github.com/sikalabs/slu/releases/download/v0.11.0-dev-2/slu_v0.11.0-dev-2_darwin_arm64.tar.gz"
+      sha256 "d54e7f431162f38171aeb4299d2371f2ea90f34881e7e8beaaf8a0f628842e24"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/sikalabs/slu/releases/download/v0.11.0-dev-1/slu_v0.11.0-dev-1_linux_amd64.tar.gz"
-      sha256 "186c82e7a627f2dc51963c9fead39ab4a38cf4c15e68c321214f6564cbeffa6f"
+      url "https://github.com/sikalabs/slu/releases/download/v0.11.0-dev-2/slu_v0.11.0-dev-2_linux_amd64.tar.gz"
+      sha256 "a223ebe9d3b4519be68c8e259376a1fff73af3dc7e1bbc53da14f7195dedae3a"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/sikalabs/slu/releases/download/v0.11.0-dev-1/slu_v0.11.0-dev-1_linux_arm64.tar.gz"
-      sha256 "238fef095f6bee13b8e5dbdc3b06315b90c284b2533988568dcf034f3b7ca9c7"
+      url "https://github.com/sikalabs/slu/releases/download/v0.11.0-dev-2/slu_v0.11.0-dev-2_linux_arm64.tar.gz"
+      sha256 "528e18562cb9b35fd9e332ebac790b24f1bd4dd9efeae630a9572105612d93a8"
     end
   end
 
