baa8b6540826efc5e86e852c05c2930626ee7572
goreleaserbot <goreleaser@carlosbecker.com>
2021-08-21 22:10:46 +0200

    Brew formula update for slu version v0.12.0-dev-1

--- Formula/slu-edge.rb
+++ Formula/slu-edge.rb
@@ -5,28 +5,28 @@
 class SluEdge < Formula
   desc "SikaLabs Utils"
   homepage "https://github.com/sikalabs/slu"
-  version "0.11.0"
+  version "0.12.0-dev-1"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/sikalabs/slu/releases/download/v0.11.0/slu_v0.11.0_darwin_amd64.tar.gz"
-      sha256 "19476f27ecb8de58c6d86ba5fadf8089eeab7a8fa26ab590b3631a13dd0112ea"
+      url "https://github.com/sikalabs/slu/releases/download/v0.12.0-dev-1/slu_v0.12.0-dev-1_darwin_amd64.tar.gz"
+      sha256 "5cf1306036c2ce9bff15550b4110da35a094397539162eaa3385a8eddc310c64"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/sikalabs/slu/releases/download/v0.11.0/slu_v0.11.0_darwin_arm64.tar.gz"
-      sha256 "16ad344a6f02d84c3e3a0fd428530173c09bd071e7d88a3d7811be5f978adbac"
+      url "https://github.com/sikalabs/slu/releases/download/v0.12.0-dev-1/slu_v0.12.0-dev-1_darwin_arm64.tar.gz"
+      sha256 "f4d00416f7e4822cf5a75e4cfde94b41939dfcd695c3dd7727303a48752bdb9d"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/sikalabs/slu/releases/download/v0.11.0/slu_v0.11.0_linux_amd64.tar.gz"
-      sha256 "8bbd1b7f7b917f9cd29ba1e5bfb9145062830a30ee9074e7a8bffd906fbc56ff"
+      url "https://github.com/sikalabs/slu/releases/download/v0.12.0-dev-1/slu_v0.12.0-dev-1_linux_amd64.tar.gz"
+      sha256 "2d1f3ccc31b3c48dbfdc8e6b2821ca050abcd3fdb3a4c96e3f905dbb01c3e1d4"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/sikalabs/slu/releases/download/v0.11.0/slu_v0.11.0_linux_arm64.tar.gz"
-      sha256 "476175835b2b4f393e31697f2f7ee4b5702b44d6aacae8d0a0d90d903b3eb360"
+      url "https://github.com/sikalabs/slu/releases/download/v0.12.0-dev-1/slu_v0.12.0-dev-1_linux_arm64.tar.gz"
+      sha256 "df89d01d62b7020459fad63c82c893a8cb63ddf11f517fb12933ea4432f1d822"
     end
   end
 
