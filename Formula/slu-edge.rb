396b0c50795605c3f404b0c49b0045db98ae8b00
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-08 21:46:30 +0200

    Brew formula update for slu version v0.14.0-dev-3

--- Formula/slu-edge.rb
+++ Formula/slu-edge.rb
@@ -5,28 +5,28 @@
 class SluEdge < Formula
   desc "SikaLabs Utils"
   homepage "https://github.com/sikalabs/slu"
-  version "0.13.0"
+  version "0.14.0-dev-3"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/sikalabs/slu/releases/download/v0.13.0/slu_v0.13.0_darwin_amd64.tar.gz"
-      sha256 "d92051d54f7bb441a76324e577d3409175d2766f3bce4acecac728cfc346149d"
+      url "https://github.com/sikalabs/slu/releases/download/v0.14.0-dev-3/slu_v0.14.0-dev-3_darwin_amd64.tar.gz"
+      sha256 "1682aad044bcc114c33eef6291b8ee326f7f2b37e1e6207ad20d56f5ba3504e8"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/sikalabs/slu/releases/download/v0.13.0/slu_v0.13.0_darwin_arm64.tar.gz"
-      sha256 "b385c8473eaedff285914df834f4be6944d00d5a83ce3309bbc288b3019e8a20"
+      url "https://github.com/sikalabs/slu/releases/download/v0.14.0-dev-3/slu_v0.14.0-dev-3_darwin_arm64.tar.gz"
+      sha256 "98d65de5b92a71b6e9a063cae2f93c0d48529721f5bee2e33ddd0e0f915231bf"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/sikalabs/slu/releases/download/v0.13.0/slu_v0.13.0_linux_amd64.tar.gz"
-      sha256 "d4a31154a3fc8fdc32a296e1a7d40389825f412b12bdb983ff2a629b68432ef7"
+      url "https://github.com/sikalabs/slu/releases/download/v0.14.0-dev-3/slu_v0.14.0-dev-3_linux_amd64.tar.gz"
+      sha256 "cfb5341ab02793673805277206eb25c586fb8153664fc0a7fc203ec7fe4a35cf"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/sikalabs/slu/releases/download/v0.13.0/slu_v0.13.0_linux_arm64.tar.gz"
-      sha256 "b3d47289873b141a9d7d7eb249d5b8a0debf8648cd6a44bba0fb6bafa9aea65b"
+      url "https://github.com/sikalabs/slu/releases/download/v0.14.0-dev-3/slu_v0.14.0-dev-3_linux_arm64.tar.gz"
+      sha256 "5cc7f22d0bf3f584ff44d71218a292d5fd153390d98b21bb5def969cb6f71ad4"
     end
   end
 
