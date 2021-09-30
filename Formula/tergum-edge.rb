e51be85b94a385f1afd1b32738501c2ba1931630
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-30 16:06:17 +0200

    Brew formula update for tergum version v0.14.0

--- Formula/tergum-edge.rb
+++ Formula/tergum-edge.rb
@@ -5,28 +5,28 @@
 class TergumEdge < Formula
   desc "Tergum Backup Tool"
   homepage "https://github.com/sikalabs/tergum"
-  version "0.13.0"
+  version "0.14.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/sikalabs/tergum/releases/download/v0.13.0/tergum_v0.13.0_darwin_amd64.tar.gz"
-      sha256 "ea1c3d19b2c48d0878546d10845a53c7b6cc2f019126b1057c464da2db99dad8"
+      url "https://github.com/sikalabs/tergum/releases/download/v0.14.0/tergum_v0.14.0_darwin_amd64.tar.gz"
+      sha256 "7b0c06171a7496a2b26a3947fe77db83f0566765a6b1811a93208100072cda34"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/sikalabs/tergum/releases/download/v0.13.0/tergum_v0.13.0_darwin_arm64.tar.gz"
-      sha256 "128f18bd0ce2e734216e0e63c34c2f1f7bae7a58e3ed20b18d71d8390f61fb0b"
+      url "https://github.com/sikalabs/tergum/releases/download/v0.14.0/tergum_v0.14.0_darwin_arm64.tar.gz"
+      sha256 "448fe967e358ee993c9bb3a312191917ae74b8af5887b5685d751b7e3fc33241"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/sikalabs/tergum/releases/download/v0.13.0/tergum_v0.13.0_linux_amd64.tar.gz"
-      sha256 "b102ab31b11a5fe2d2c79b66081587b5f566ccca10fc1b4c8adb1112b4f65dd1"
+      url "https://github.com/sikalabs/tergum/releases/download/v0.14.0/tergum_v0.14.0_linux_amd64.tar.gz"
+      sha256 "0810002484c53ab2b3c0e97d40cc0e58b38f27b269494402fce096156f78315f"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/sikalabs/tergum/releases/download/v0.13.0/tergum_v0.13.0_linux_arm64.tar.gz"
-      sha256 "75d7160e4a4cf5dfb406ede371e75b1191e24a2121f13ffe6efa24a0e353493d"
+      url "https://github.com/sikalabs/tergum/releases/download/v0.14.0/tergum_v0.14.0_linux_arm64.tar.gz"
+      sha256 "8822122bc21d4f5333ed3a165aced56bd5aac01e3f99ef12d854658a4d3fba59"
     end
   end
 
