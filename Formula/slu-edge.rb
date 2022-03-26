43ddfffd78c1d2dc0d37ba7cafbc22c89c7ecd50
goreleaserbot <goreleaser@carlosbecker.com>
2022-03-26 19:56:59 +0100

    Brew formula update for slu version v0.36.0

--- Formula/slu-edge.rb
+++ Formula/slu-edge.rb
@@ -5,20 +5,20 @@
 class SluEdge < Formula
   desc "SikaLabs Utils"
   homepage "https://github.com/sikalabs/slu"
-  version "0.36.0-dev-4"
+  version "0.36.0"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/sikalabs/slu/releases/download/v0.36.0-dev-4/slu_v0.36.0-dev-4_darwin_arm64.tar.gz"
-      sha256 "d35c77177240f4cb4f073554d14ec6cfd076354c7f9fbc989152f73adc508c6e"
+    if Hardware::CPU.intel?
+      url "https://github.com/sikalabs/slu/releases/download/v0.36.0/slu_v0.36.0_darwin_amd64.tar.gz"
+      sha256 "e3e9de51d3b3b25802fdc5d830168567118770b975db49c66d3e9b41f3467a25"
 
       def install
         bin.install "slu"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/sikalabs/slu/releases/download/v0.36.0-dev-4/slu_v0.36.0-dev-4_darwin_amd64.tar.gz"
-      sha256 "c09dcf9244b1b897aeab01babf1151b9bb2586a6c5f4464ce81745c503c3490d"
+    if Hardware::CPU.arm?
+      url "https://github.com/sikalabs/slu/releases/download/v0.36.0/slu_v0.36.0_darwin_arm64.tar.gz"
+      sha256 "3e5c2dc49562ed7ae81eb75ced21293331502ddb6308cc44302a1b4a809938b2"
 
       def install
         bin.install "slu"
@@ -28,16 +28,16 @@ class SluEdge < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/sikalabs/slu/releases/download/v0.36.0-dev-4/slu_v0.36.0-dev-4_linux_arm64.tar.gz"
-      sha256 "95d4ed532e49a61527956146e040d025212853544e94a1aa057fd20d28198c49"
+      url "https://github.com/sikalabs/slu/releases/download/v0.36.0/slu_v0.36.0_linux_arm64.tar.gz"
+      sha256 "0c93c4acd8fe6e314b7a1a967f3773398e25273bc3858fc6a4895fc3fc073ee9"
 
       def install
         bin.install "slu"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/sikalabs/slu/releases/download/v0.36.0-dev-4/slu_v0.36.0-dev-4_linux_amd64.tar.gz"
-      sha256 "f04bfe943d0df8b99d24e7107bf0fd7e3e190ec46ef75acfba2b8a4f932ae2c8"
+      url "https://github.com/sikalabs/slu/releases/download/v0.36.0/slu_v0.36.0_linux_amd64.tar.gz"
+      sha256 "75c9ae6a7186cce296aa055117060cc4d500434ac1c8100e3e9097824fc79d59"
 
       def install
         bin.install "slu"
