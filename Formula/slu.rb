a003e0f53c8dd8ce22da37b14e50d3c963e6bce3
goreleaserbot <goreleaser@carlosbecker.com>
2022-04-06 15:42:19 +0200

    Brew formula update for slu version v0.38.0

--- Formula/slu.rb
+++ Formula/slu.rb
@@ -5,20 +5,20 @@
 class Slu < Formula
   desc "SikaLabs Utils"
   homepage "https://github.com/sikalabs/slu"
-  version "0.37.0"
+  version "0.38.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/sikalabs/slu/releases/download/v0.37.0/slu_v0.37.0_darwin_arm64.tar.gz"
-      sha256 "d2607c3edff94cf578cf51b1ad96108eef1fef4b3242ee279bc6e8a22aad531f"
+      url "https://github.com/sikalabs/slu/releases/download/v0.38.0/slu_v0.38.0_darwin_arm64.tar.gz"
+      sha256 "9a7242fdfd117fe4b3540ca8df0daf86f7a4c816462d9066563b0b970a96d137"
 
       def install
         bin.install "slu"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/sikalabs/slu/releases/download/v0.37.0/slu_v0.37.0_darwin_amd64.tar.gz"
-      sha256 "9fd29881e25455ff14f3564d36514979f8e66892dbfa1eaed94e8a94b294bc26"
+      url "https://github.com/sikalabs/slu/releases/download/v0.38.0/slu_v0.38.0_darwin_amd64.tar.gz"
+      sha256 "52229fd1a2c8e6becce6a4e5421516b55fb8a12254be984a386fee1c18b46563"
 
       def install
         bin.install "slu"
@@ -28,16 +28,16 @@ class Slu < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/sikalabs/slu/releases/download/v0.37.0/slu_v0.37.0_linux_arm64.tar.gz"
-      sha256 "0c7fdf9fdfe93931eaf42f0cac7a199c2ca0a4658d134be94ae6581244d185c8"
+      url "https://github.com/sikalabs/slu/releases/download/v0.38.0/slu_v0.38.0_linux_arm64.tar.gz"
+      sha256 "720b75e7c794eb227dcb0bd98f952beb15d6f2a6bb27c168dfb58e1013be51bb"
 
       def install
         bin.install "slu"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/sikalabs/slu/releases/download/v0.37.0/slu_v0.37.0_linux_amd64.tar.gz"
-      sha256 "a82760d17a06d661f3af264654fa7a4fe1bb41ed385626df07159df646fc6269"
+      url "https://github.com/sikalabs/slu/releases/download/v0.38.0/slu_v0.38.0_linux_amd64.tar.gz"
+      sha256 "24385b042edb27a3f07705be5b85f0bbaba93902823f70f2107b314abcdbdd6d"
 
       def install
         bin.install "slu"
